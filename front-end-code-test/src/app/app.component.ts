df42625d5127b5f2c831010f199a30fc99de1cc2
Alvaro Andres Bedoya Gomez <andresbedoyagomez@gmail.com>
2022-02-09 17:41:53 -0500

    Probando bolas animadas

--- front-end-code-test/src/app/app.component.ts
+++ front-end-code-test/src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import {trigger, state, style, animate, transition} from '@angular/animations';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations:
+  [
+    trigger('triggerBackgroundChange',
+    [
+      state('fisrt', style({'background-color':'yellow','color':'black'})),
+      state('second', style({'background-color':'red', 'color':'white'})),
+      transition('*=>*',animate(3000))
+    ])
+  ]
 })
+
 export class AppComponent {
   title = 'Winning Balls';
+  color: string= 'first';
+  changeColor(){
+    if(this.color=='first')
+      this.color='second';
+    else
+    this.color='first';
+  }
 }
