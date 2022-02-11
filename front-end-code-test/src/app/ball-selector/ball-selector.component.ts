ad9be0b6d82fc396976f035d27a38519729beb71
Alvaro Andres Bedoya Gomez <andresbedoyagomez@gmail.com>
2022-02-11 17:52:50 -0500

    reparado

--- front-end-code-test/src/app/ball-selector/ball-selector.component.ts
+++ front-end-code-test/src/app/ball-selector/ball-selector.component.ts
@@ -8,11 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BallSelectorComponent implements OnInit {
   balls: any[] = [];
-  ballSelect: number = 0;
   constructor() { }
 
   ngOnInit(): void {
-    this.balls=[1,2,3,4,5,6,7,8,9,10]
+    this.balls=[0,1,2,3,4,5,6,7,8,9]
   }
 
 }
